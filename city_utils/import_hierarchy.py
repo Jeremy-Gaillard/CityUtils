a890ebe8c696e6c057cec5b2d784833c895d7dac
Jérémy Gaillard <jeremy.c.gaillard.pro@gmail.com>
2017-08-17 16:17:44 +0200

    Various hierarchy import improvement:
    * Hierarchy import no longer requires explicit links between levels
    * The different levels do not require to match exactly, they will be realigned if needed
    * Automatically merge tiles that share a city object
    * Compute building footprint
    * Faster indexation

--- city_utils/import_hierarchy.py
+++ city_utils/import_hierarchy.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
     """
     # Temp hardwritten configuration
     db = psycopg2.connect(
-        "postgresql://jeremy:jeremy@localhost:5432/lyon"
+        "postgresql://hme:jeremy@localhost:5432/lyon"
     )
     db.autocommit = True
     cursor = db.cursor()
@@ -73,12 +73,17 @@ if __name__ == '__main__':
     t_hierarchy = "test.hierarchy"
     t_tiles = "test.tiles"
     t_buildings = "test.buildings"
-    t_buildings_data = "test.lod2"
+    t_buildings_data = "split_mp_roofs"
     epsg = 3946
     db_name = "lyon"
-    shp = ["test_files/Lyon/Ilots_reseauroutier_v3.shp test.tiles",
+    """shp = ["test_files/Lyon/Ilots_reseauroutier_v3.shp test.tiles",
         "test_files/Lyon/Ilots_reseauroutier_v2.shp test.tiles",
-        "test_files/Lyon/Ilots_reseauroutier.shp test.tiles"]
+        "test_files/Lyon/Ilots_reseauroutier.shp test.tiles"]"""
+
+    shp = ["/home/hme/data/shp/resultat-adr_voie_lieu.adrarrond/adr_voie_lieu.adrarrond.shp",
+           "/home/hme/data/shp/resultat-vdl_vie_citoyenne.perimetre_de_quartier/vdl_vie_citoyenne.perimetre_de_quartier.shp",
+           "/home/hme/data/shp/resultat-vdl_vie_citoyenne.contour_de_bureau_de_vote/vdl_vie_citoyenne.contour_de_bureau_de_vote.shp",
+           "/home/hme/data/shp/routes/routes.shp"]
 
     # Drop tables
     query = "DROP TABLE IF EXISTS {0}".format(t_buildings)
@@ -107,6 +112,7 @@ if __name__ == '__main__':
         subprocess.call(("psql", db_name), stdin=pgsql.stdout)
         query = "UPDATE {0} SET depth={1} WHERE depth is null".format(t_tiles, str(i))
         cursor.execute(query)
+    maxDepth = len(shp) - 1
 
     # Rename columns
     query = "ALTER TABLE {0} RENAME COLUMN geom TO footprint".format(t_tiles)
@@ -116,27 +122,6 @@ if __name__ == '__main__':
     query = "CREATE INDEX tiles_footprint_idx ON {0} using gist(footprint)".format(t_tiles)
     cursor.execute(query)
 
-    # Create hierarchy
-    query = "CREATE TABLE {0} (tile serial, child integer)".format(t_hierarchy)
-    cursor.execute(query)
-    query = "CREATE INDEX hierarchy_tile_idx ON {0} (tile)".format(t_hierarchy)
-    cursor.execute(query)
-
-    query = "SELECT tile, fid, id_parent, depth from {0}".format(t_tiles)
-    cursor.execute(query)
-
-    parentOf = {}
-    fidToGid = {}
-    for gid, fid, parentId, depth in cursor:
-        fidToGid[(int(fid), depth)] = gid
-        if parentId != None:
-            parentOf[gid] = (int(parentId), depth - 1)
-
-    tileChild = [(fidToGid[parentOf[gid]], gid) for gid in parentOf]
-    query = "INSERT INTO {0}".format(t_hierarchy)
-    # Suboptimal, see http://stackoverflow.com/questions/8134602/psycopg2-insert-multiple-rows-with-one-query
-    cursor.executemany(query + " VALUES (%s, %s)", tileChild)
-
     # Create building table
     query = "CREATE TABLE {0} (gid serial, tile integer, footprint geometry(Multipolygon, {1}))".format(t_buildings, epsg)
     cursor.execute(query)
@@ -145,11 +130,88 @@ if __name__ == '__main__':
     query = "CREATE INDEX buildings_tile_idx ON {0} (tile)".format(t_buildings)
     cursor.execute(query)
 
+    # Populate building table and compute footprints
+    query = "INSERT INTO {0} (gid, footprint) SELECT gid, ST_Multi(ST_Buffer(St_Force2D(geom), 0)) FROM {1}".format(t_buildings, t_buildings_data)
+    cursor.execute(query)
+    query = ("UPDATE {0} t SET footprint = a.geom FROM ("
+                " SELECT gid, ST_Collect(ST_MakePolygon(geom)) AS geom"
+                " FROM ("
+                    "SELECT gid, ST_NRings(footprint) AS nrings,"
+                        "ST_ExteriorRing((ST_Dump(footprint)).geom) AS geom"
+                    " FROM {0}"
+                    " WHERE ST_NRings(footprint) > 1"
+                    ") s"
+                " GROUP BY gid, nrings"
+                " HAVING nrings > COUNT(gid)"
+                ") a"
+            " WHERE t.gid = a.gid;").format(t_buildings)
+    cursor.execute(query)
+
+    # Check for building-tile interserctions
+    query = ("WITH t AS (SELECT tile, footprint FROM {0} WHERE depth={1}),"
+             " d AS (SELECT t.tile, {2}.gid FROM t, {2} WHERE st_intersects("
+             "t.footprint, {2}.footprint)), e AS (SELECT gid, count(*) AS cnt"
+             " FROM d GROUP BY gid) SELECT d.gid, d.tile FROM d JOIN e ON "
+             " d.gid=e.gid WHERE cnt>1;").format(t_tiles, maxDepth, t_buildings)
+    cursor.execute(query)
+    gid2Tiles = {}
+    tile2Group = {}
+    tileGroups = []
+    for (gid, tile) in cursor:
+        if gid not in gid2Tiles:
+            gid2Tiles[gid] = []
+        gid2Tiles[gid].append(tile)
+    for gid in gid2Tiles:
+        group = { tile for tile in gid2Tiles[gid] }
+        for tile in gid2Tiles[gid]:
+            if tile in tile2Group:
+                if tile2Group[tile] in tileGroups:
+                    tileGroups.remove(tile2Group[tile])
+                group.update(tile2Group[tile])
+            tile2Group[tile] = group
+        tileGroups.append(group)
+
+    # Merge tiles with overlapping objects
+    for group in tileGroups:
+        condition = " OR ".join(["tile={0}".format(tile) for tile in group])
+        query = "INSERT INTO {0} (footprint, depth) SELECT ST_Multi(ST_Union(footprint)), {2} FROM {0} WHERE {1}".format(t_tiles, condition, maxDepth)
+        cursor.execute(query)
+        query = "DELETE FROM {0} WHERE {1}".format(t_tiles, condition)
+        cursor.execute(query)
+
+    # Create hierarchy
+    query = "CREATE TABLE {0} (tile serial, child integer)".format(t_hierarchy)
+    cursor.execute(query)
+    query = "CREATE INDEX hierarchy_tile_idx ON {0} (tile)".format(t_hierarchy)
+    cursor.execute(query)
+
+    # Link children with their parent
+    for i in range(maxDepth):
+        query = ("WITH t AS (SELECT * FROM {0} WHERE depth={1}),"
+                "d AS (SELECT * FROM {0} WHERE depth={1}+1)"
+                "SELECT t.tile, d.tile FROM t, d WHERE ST_Intersects("
+                "ST_Centroid(d.footprint), t.footprint)").format(t_tiles, i)
+        cursor.execute(query)
+        results = cursor.fetchall()
+        for parent, child in results:
+            query = "INSERT INTO {0} (tile, child) VALUES ({1}, {2})".format(
+                    t_hierarchy, parent, child)
+            cursor.execute(query)
+
+    # Snap parent tile boundaries to the ones of their children
+    # TODO: keep make valid ?
+    for i in reversed(range(maxDepth)):
+        query = ("WITH t AS (SELECT {1}.tile, ST_Union(ST_MakeValid(footprint))"
+                " AS reference FROM {0} JOIN {1} ON {0}.tile={1}.child WHERE"
+                " depth={2} GROUP BY {1}.tile) UPDATE {0} SET footprint="
+                "ST_Multi(ST_Buffer(ST_Buffer(reference, 2),-2)) FROM t"
+                " WHERE {0}.tile=t.tile").format(t_tiles, t_hierarchy, i + 1)
+        cursor.execute(query)
+
     # Building indexation
     print("Indexing")
-    maxDepth = len(shp) - 1
-    query = ("WITH d AS (SELECT tile, footprint FROM {0} WHERE depth={4})"
-            "INSERT INTO {1} (gid, tile) SELECT gid, d.tile FROM {2} INNER JOIN d ON ST_Intersects(d.footprint, ST_Centroid(ST_SetSRID(Box2D({2}.geom), {3})));".format(t_tiles, t_buildings, t_buildings_data, epsg, maxDepth))
+    query = ("WITH d AS (SELECT tile, footprint FROM {0} WHERE depth={2})"
+            "UPDATE {1} SET tile=d.tile FROM d WHERE ST_Intersects(d.footprint, {1}.footprint)").format(t_tiles, t_buildings, maxDepth)
     cursor.execute(query)
 
     # Compute bbox
